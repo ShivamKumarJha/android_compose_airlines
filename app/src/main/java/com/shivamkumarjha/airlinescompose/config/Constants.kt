3da3ff0023684747232da0800fedd5f25f7025ba
ShivamKumarJha <jha.shivam3@gmail.com>
2021-09-07 00:13:25 +0530

    Add Constants
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ app/src/main/java/com/shivamkumarjha/airlinescompose/config/Constants.kt
@@ -0,0 +1,12 @@
+package com.shivamkumarjha.airlinescompose.config
+
+object Constants {
+
+    const val TAG = "Airlines"
+
+    //API
+    const val BASE_URL = "https://api.instantwebtools.net/v1/"
+
+    //Database
+    const val DB_NAME = "app_database.db"
+}
